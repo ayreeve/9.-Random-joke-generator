9de40e693372658fc4ac67cbe4cb869ce9f94fbf
Kaja <ayreeve@wp.pl>
2017-05-28 12:27:06 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// jslint devel: true
+
+// server request
+var url = 'http://api.icndb.com/jokes/random',
+    button = document.getElementById('get-joke'),
+    paragraph = document.getElementById('joke');
+
+function getJoke() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function () {
+        var response = JSON.parse(xhr.response);
+        paragraph.innerHTML = response.value.joke;
+    });
+    xhr.send();
+}
+
+window.onload = function () {
+    getJoke();
+};
+
+button.addEventListener('click', function () {
+    getJoke();
+});
